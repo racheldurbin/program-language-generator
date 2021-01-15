06c24be231626807908275588f0955199e04b1ba
Rachel Durbin <cesenarachel@gmail.com>
2021-01-15 11:17:20 -0800

    researching how to get response from radio button input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault();
     const personInput = $("input#person").val();
+    console.log(heightInput);
+    if (heightInput >= 48) {
+      $(".tallRides").addClass("highlightRide");
+      $(".mediumRides").addClass("highlightRide");
+      $(".shortRides").addClass("highlightRide");
 
     $(".person").text(personInput);
 
     $("#responseToUser").show();
 
-    event.preventDefault();
+
   });
 });
