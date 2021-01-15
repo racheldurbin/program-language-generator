53f05fab627a818545ce6cf46b0157da277f690d
Rachel Durbin <cesenarachel@gmail.com>
2021-01-15 12:05:59 -0800

    show I am really struggling to find out how to get my radio buttons to form a response for user

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,10 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const personInput = $("input#person").val();
-    console.log(heightInput);
-    if (heightInput >= 48) {
-      $(".tallRides").addClass("highlightRide");
-      $(".mediumRides").addClass("highlightRide");
-      $(".shortRides").addClass("highlightRide");
 
     $(".person").text(personInput);
 
     $("#responseToUser").show();
-
-
+    
   });
 });
