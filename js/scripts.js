4bb264ee20114e2209a9b09b13f189067ef00a83
Rachel Durbin <cesenarachel@gmail.com>
2021-01-15 12:23:06 -0800

    create const in js code

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
 
     $(".person").text(personInput);
 
+    const type = $("input:radio[name=type]:checked").val();
+    const type = $("input:radio[name=type]:checked").val();
+    const type = $("input:radio[name=type]:checked").val();
+
+    const simpleResponse = $("input:radio[name=simpleResponse]:checked").val();
+    const simpleResponse = $("input:radio[name=simpleResponse]:checked").val();
+    const simpleResponse = $("input:radio[name=simpleResponse]:checked").val();
+
+    const relationship = $("input:radio[name=relationship]:checked").val();
+    const relationship = $("input:radio[name=relationship]:checked").val();
+    const relationship = $("input:radio[name=relationship]:checked").val();
+
+
     $("#responseToUser").show();
     
   });
